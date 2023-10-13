573741fe19c8535d3e838e1ce16912c97e342e68
Caz92102 <147732904+Caz92102@users.noreply.github.com>
2023-10-13 21:54:34 +0200

    Update matter.js

--- matter.js
+++ matter.js
@@ -244,8 +244,7 @@ function initSimulation() {
     }
   });
   var tagPhotography = Bodies.rectangle(
-    containerWidth / 2 - 74, 540, 248, 56,
-    {
+    containerWidth / 2 - 74, 540, 248, 56, {
       chamfer: { radius: radius },
       render: {
         sprite: {
@@ -258,8 +257,7 @@ function initSimulation() {
     }
   });
   var tagVideography = Bodies.rectangle(
-    containerWidth / 2 - 74, 540, 248, 56,
-    {
+    containerWidth / 2 - 74, 540, 248, 56, {
       chamfer: { radius: radius },
       render: {
         sprite: {
