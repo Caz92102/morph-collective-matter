f249f91e70f99f306427a9d6822dc4d417df50d7
Caz92102 <147732904+Caz92102@users.noreply.github.com>
2023-10-13 22:02:05 +0200

    Update matter.js

--- matter.js
+++ matter.js
@@ -185,17 +185,7 @@ function initSimulation() {
       }
     }
   });
-  var tagAnimation = Bodies.rectangle(containerWidth / 59, 260, 172, 56, {
-    chamfer: { radius: radius },
-    render: {
-      sprite: {
-        texture:
-          "https://uploads-ssl.webflow.com/650833d0628374915846188d/652993cab48d07bbf90bfa37_animation-matter.svg",
-        xScale: 1,
-        yScale: 1
-      }
-    }
-  });
+ 
   // Branding
   var tagProjectManagement = Bodies.rectangle(containerWidth / 2 + 150, 500, 164, 56, {
     chamfer: { radius: radius },
@@ -309,17 +299,7 @@ function initSimulation() {
       }
     }
   });
-  var tagProduction = Bodies.rectangle(containerWidth / 59, 260, 172, 56, {
-    chamfer: { radius: radius },
-    render: {
-      sprite: {
-        texture:
-          "https://uploads-ssl.webflow.com/650833d0628374915846188d/65299e0cb910c6e29934501f_production.svg",
-        xScale: 1,
-        yScale: 1
-      }
-    }
-  });
+
   // add all of the bodies to the world
   World.add(engine.world, [
     ground,
